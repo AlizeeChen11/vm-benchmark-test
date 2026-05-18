858442820c89b20fbdff555c65d4777f6eea8841
Elsa Chen <142569415+AlizeeChen11@users.noreply.github.com>
2026-05-18 10:27:56 +0800

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install lshw build-essential fio iperf3 stress-ng git  -y
+
+git clone https://github.com/intel/lmbench.git
+cd lmbench
+make
+find . -name lat_mem_rd
+
+
+wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
+gcc -O2 -fopenmp -DSTREAM_ARRAY_SIZE=80000000 -DNTIMES=100 stream.c -o stream
+
+wget https://github.com/KCORES/core-to-core-latency-plus/releases/download/0.1.17/core-to-core-latency-plus-linux-amd64
+chmod +x core-to-core-latency-plus-linux-amd64
+
+
